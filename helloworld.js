741b54c68c296c30658de4c6bc0891a60a905732
stlbpw <69367908+stlbpw@users.noreply.github.com>
2024-09-22 19:07:15 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,4 @@
 // Console will print the message
-console.log('Hello World From Bruce - Manual\n')
+console.log('\n\nHello World From Bruce - Manual\n')
+console.log('Commit 5\n')
 process.exit(0);
